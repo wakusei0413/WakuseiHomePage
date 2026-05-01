8de90c8572e0a67eb89a0332c6a1aa5fda0cd59b
wakusei0413 <wakusei0413@users.noreply.github.com>
2026-05-01 23:12:38 +0800

    stage03

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import solid from '@astrojs/solid-js';
 export default defineConfig({
     integrations: [solid()],
     output: 'static',
-    outDir: './dist'
+    outDir: './dist',
+    devToolbar: {
+        enabled: false
+    }
 });
