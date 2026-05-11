b5cae72d27b50aba020ef34ddb5191d44acbf5da
wakusei0413 <79008084+wakusei0413@users.noreply.github.com>
2026-05-11 23:54:16 +0800

    feat: migrate simple components to Vue (Icon, Footer, ClockPanel, TypewriterSlogan)

---
+++ src/components/ClockPanel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useTime } from '../composables/useTime';
+import type { TimeConfig } from '../types/site';
+
+const props = defineProps<{
+    config: TimeConfig;
+}>();
+
+const { timeString, dateParts } = useTime(props.config);
+</script>
+
+<template>
+    <div class="clock">{{ timeString }}</div>
+    <div v-if="config.showWeekday" class="weekday">{{ dateParts.weekday }}</div>
+    <div v-if="config.showDate && dateParts.dateDisplay" class="date-display">{{ dateParts.dateDisplay }}</div>
+</template>
